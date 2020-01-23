7d6aa78ccc8ec8ab29dcea4ce7d27de4cb9c4de1
UrielKaralden <urielkaralden@gmail.com>
2020-01-23 20:03:45 +0100

    Added GameFolder model

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/GameFolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    class GameFolder
+    {
+        public string DisplayName { get; set; }
+        public GameFolder(string item)
+        {
+            DisplayName = item;
+        }
+    }
+}
