8395e1248bf15f9ca8c8bfbed1346f139a7e3915
UrielKaralden <urielkaralden@gmail.com>
2020-01-23 20:04:48 +0100

    Added RdfElement model

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/RdfElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    class RdfElement
+    {
+        public string DisplayName { get; set; }
+        public RdfElement(string item)
+        {
+            //uri = new Uri(item);
+            //int index = item.LastIndexOf('#');
+            //name = item.Substring(index + 1).Replace("_", " ").Trim();
+            DisplayName = item;
+        }
+    }
+}
