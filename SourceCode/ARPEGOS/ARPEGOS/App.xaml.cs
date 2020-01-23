61e4fb011d35e07284b4f8be33167cda4bf09346
UrielKaralden <urielkaralden@gmail.com>
2020-01-23 20:26:36 +0100

    Beginning app loading ExtendedLoadingPage

--- SourceCode/ARPEGOS/ARPEGOS/App.xaml.cs
+++ SourceCode/ARPEGOS/ARPEGOS/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARPEGOS.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace ARPEGOS
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ExtendedLoadingPage());
         }
 
         protected override void OnStart()
