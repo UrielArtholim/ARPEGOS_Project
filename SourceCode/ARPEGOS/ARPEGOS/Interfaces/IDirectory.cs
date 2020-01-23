4f7a0a922e12ab301e0cb00e135bbc86f4e9b60a
UrielKaralden <urielkaralden@gmail.com>
2020-01-23 19:49:04 +0100

    Added Interface IDirectory

---
+++ SourceCode/ARPEGOS/ARPEGOS/Interfaces/IDirectory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Interfaces
+{
+    public interface IDirectory
+    {
+        string CreateDirectory(string directoryName);
+        string CreateDirectory(string rootDirectoryName, string directoryName);
+        string RenameDirectory(string oldDirectoryName, string newDirectoryName);
+        void RemoveDirectory(string directoryName);
+        void ClearBaseDirectory();
+    }
+}
