2f8b4570edf4474af9ed8375dac444f4af1cabbe
UrielKaralden <urielkaralden@gmail.com>
2020-01-23 20:35:06 +0100

    Added iOS DirectoryService empty implementation

---
+++ SourceCode/ARPEGOS/ARPEGOS.iOS/Services/DirectoryService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ARPEGOS.Interfaces;
+using ARPEGOS.iOS.Services;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(DirectoryService))]
+
+namespace ARPEGOS.iOS.Services
+{
+    class DirectoryService : IDirectory
+    {
+        public void ClearBaseDirectory()
+        {
+
+        }
+
+        public string CreateDirectory(string directoryName)
+        {
+            return null;
+        }
+
+        public string CreateDirectory(string rootDirectoryName, string directoryName)
+        {
+            return null;
+        }
+
+        public void RemoveDirectory(string directoryName)
+        {
+            
+        }
+
+        public string RenameDirectory(string oldDirectoryName, string newDirectoryName)
+        {
+            return null;
+        }
+    }
+}
