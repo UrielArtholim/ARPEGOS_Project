cb03f26843d072b294d3a63d6e6ec0a2830ab42e
UrielKaralden <urielkaralden@gmail.com>
2020-01-24 14:21:52 +0100

    Added LoginManager interface

---
+++ SourceCode/ARPEGOS/ARPEGOS/Interfaces/ILoginManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Interfaces
+{
+    public interface ILoginManager
+    {
+        void ShowMainPage();
+        void Logout();
+    }
+}
