47e059c74384264867330269754a836ff962ef05
UrielKaralden <urielkaralden@gmail.com>
2020-01-24 18:14:31 +0100

    Added ItemGroup model

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/ItemGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    public class ItemGroup : ObservableCollection<SimpleListItem>, INotifyPropertyChanged
+    {
+        private bool groupExpanded;
+        public string groupName { get; set; }
+        public ItemGroup( string name, bool expanded = false)
+        {
+            groupName = name;
+            groupExpanded = expanded;
+        }
+    }
+}
