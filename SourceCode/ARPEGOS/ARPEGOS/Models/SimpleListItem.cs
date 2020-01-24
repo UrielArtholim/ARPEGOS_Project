ea2b15faaae1d09582ea4f77575957945e138153
UrielKaralden <urielkaralden@gmail.com>
2020-01-24 12:56:56 +0100

    Changed GameFolder to SimpleListItem

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/SimpleListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    class SimpleListItem
+    {
+        public string DisplayName { get; set; }
+        public SimpleListItem(string item)
+        {
+            DisplayName = item;
+        }
+    }
+}
