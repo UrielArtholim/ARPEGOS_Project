3be3c33b1858b63a89c6c9fc152fe38169a281f7
UrielKaralden <urielkaralden@gmail.com>
2020-01-25 13:12:03 +0100

    Added GameFile model

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/GameFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    public class GameFile
+    {
+        public string Name { get; private set; }
+        public string GamePath { get; private set; }
+
+        public GameFile(string path)
+        {
+            GamePath = path;
+            Name = Path.GetDirectoryName(GamePath);
+        }
+    }
+}
