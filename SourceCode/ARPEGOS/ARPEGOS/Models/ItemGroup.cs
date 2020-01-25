819dc7f0a9b25280d29720ba19e3a32367a96082
UrielKaralden <urielkaralden@gmail.com>
2020-01-25 13:13:26 +0100

    Added ItemGroup model

--- SourceCode/ARPEGOS/ARPEGOS/Models/ItemGroup.cs
+++ SourceCode/ARPEGOS/ARPEGOS/Models/ItemGroup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectSetup.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -6,14 +7,33 @@ using System.Text;
 
 namespace ARPEGOS.Models
 {
-    public class ItemGroup : ObservableCollection<SimpleListItem>, INotifyPropertyChanged
+    public class ItemGroup : ObservableCollection<SimpleListItem>
     {
         private bool groupExpanded;
-        public string groupName { get; set; }
+        public event PropertyChangedEventHandler ExpandedPropertyChanged;
+        public string GroupName { get; set; }
         public ItemGroup( string name, bool expanded = false)
         {
-            groupName = name;
+            GroupName = name;
             groupExpanded = expanded;
+        }
+        public bool GroupExpanded
+        {
+            get 
+            { 
+                return groupExpanded; 
+            }
+            set 
+            { 
+                if (groupExpanded != value)
+                    groupExpanded = value;
+                OnPropertyChanged("Expanded");
+            }
+        }
+
+        protected void OnPropertyChanged(string groupExpanded)
+        {
+            ExpandedPropertyChanged?.Invoke(this, new PropertyChangedEventArgs(groupExpanded));
         }
     }
 }
