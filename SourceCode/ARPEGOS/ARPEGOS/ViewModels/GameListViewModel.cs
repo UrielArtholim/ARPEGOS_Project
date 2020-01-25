e4368dce34d682f96261d16a9eb74a5efefc4267
UrielKaralden <urielkaralden@gmail.com>
2020-01-25 20:53:55 +0100

    Updated class inheritance and CheckGames method

--- SourceCode/ARPEGOS/ARPEGOS/ViewModels/GameListViewModel.cs
+++ SourceCode/ARPEGOS/ARPEGOS/ViewModels/GameListViewModel.cs
@@ -6,37 +6,73 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ARPEGOS.ViewModels
 {
-    class GameListViewModel
+    class GameListViewModel : ListViewModel
     {
-        static readonly string gamesRootDirectoryName = "Games";
-        static readonly IDirectory directoryHelper = DependencyService.Get<IDirectory>();
-        static readonly string gamesRootDirectoryPath = directoryHelper.CreateDirectory(gamesRootDirectoryName);
-
+        static readonly string rootDirectoryName = "Games";
+        readonly string gamesRootDirectoryPath;
         public IList<SimpleListItem> GameList { get; private set; }
         public GameListViewModel()
         {
             GameList = new ObservableCollection<SimpleListItem>();
+            gamesRootDirectoryPath = Path.Combine(directoryHelper.GetBaseDirectory(), rootDirectoryName);
+
+            if (!Directory.Exists(gamesRootDirectoryPath))
+                directoryHelper.CreateDirectory(gamesRootDirectoryPath);
+
             CheckGames();
-            GetGameList();
+            GetList();
         }
         void CheckGames()
         {
+            DirectoryInfo gamesRootDirectoryInfo = new DirectoryInfo(gamesRootDirectoryPath);
+            var subdirectories = gamesRootDirectoryInfo.GetDirectories();
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+            var ResourceNames = assembly.GetManifestResourceNames();
+            string[] gamefiles = { };
+            gamefiles = ResourceNames.Where(x => x.EndsWith(".owl")).ToArray();
+            foreach (var game in gamefiles)
+            {
+                var filefullPath = game.Split('.');
+                Debug.WriteLine("");
+                var folderName = filefullPath[2].Replace('_',' ');
+                var fileName = filefullPath[3] + "." + filefullPath[4];
+                var folderPath = Path.Combine(gamesRootDirectoryPath, folderName);
+                var filePath = Path.Combine(gamesRootDirectoryPath, folderName, fileName);
+
+                if (!Directory.Exists(folderPath))
+                    directoryHelper.CreateDirectory(folderPath);
+
+                if (!File.Exists(filePath))
+                    WriteResourceToFile(game, filePath);
+            }
         }
-        public void GetGameList()
+
+        void WriteResourceToFile(string resourcePath, string filePath)
+        {
+            using(var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcePath))
+            {
+                using(var file = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    resource.CopyTo(file);
+                }
+            }
+        }
+        public override void GetList()
         {
             DirectoryInfo gamesRootDirectoryInfo = new DirectoryInfo(gamesRootDirectoryPath);
             foreach (DirectoryInfo gameDirectory in gamesRootDirectoryInfo.GetDirectories())
             {
                 GameList.Add(new SimpleListItem(gameDirectory.Name));
             }
-
         }
     }
 }
