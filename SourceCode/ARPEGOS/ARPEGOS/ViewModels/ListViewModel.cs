1ae79fb297b1dabfe02d3f3ea81897210383b490
UrielKaralden <urielkaralden@gmail.com>
2020-01-25 13:35:08 +0100

    Added ListViewModel abstract class

---
+++ SourceCode/ARPEGOS/ARPEGOS/ViewModels/ListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ARPEGOS.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ARPEGOS.ViewModels
+{
+    public abstract class ListViewModel
+    {
+        static readonly IDirectory directoryHelper = DependencyService.Get<IDirectory>();
+
+        public abstract void GetList();
+    }
+}
