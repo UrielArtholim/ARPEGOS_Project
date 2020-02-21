f6f62f3a24373a0dbc857b712983b2d0c1be19d3
UrielKaralden <urielkaralden@gmail.com>
2020-02-21 12:11:23 +0100

    Added CreateCharacterPage

---
+++ SourceCode/ARPEGOS/ARPEGOS/Views/CreateCharacterPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ARPEGOS.Views
+{
+    using ARPEGOS.Models;
+    using ARPEGOS.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateCharacterPage : ContentPage
+    {
+        readonly CreateCharacterViewModel viewModel;
+        public CreateCharacterPage()
+        {
+            this.InitializeComponent();
+            viewModel = new CreateCharacterViewModel();
+            this.BindingContext = viewModel;
+        }
+
+
+    }
+}
