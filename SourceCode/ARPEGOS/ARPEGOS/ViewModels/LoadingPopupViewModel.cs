e16b94c10918f29393084ecec7ff26e4010df6b1
UrielKaralden <urielkaralden@gmail.com>
2020-02-21 12:10:48 +0100

    Added LoadingPopup View Model

---
+++ SourceCode/ARPEGOS/ARPEGOS/ViewModels/LoadingPopupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.ViewModels
+{
+    public class LoadingPopupViewModel
+    {
+        string LoadingText { get; set; }
+        public LoadingPopupViewModel(string text)
+        {
+            LoadingText = text;
+        }
+    }
+}
