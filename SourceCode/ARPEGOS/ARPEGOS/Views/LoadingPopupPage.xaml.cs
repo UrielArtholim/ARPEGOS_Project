00ed1c32d93fb7025b23b8f7b0dc90d879b9083d
UrielKaralden <urielkaralden@gmail.com>
2020-02-21 12:10:20 +0100

    Added LoadingPopupPage

---
+++ SourceCode/ARPEGOS/ARPEGOS/Views/LoadingPopupPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ARPEGOS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ARPEGOS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadingPopupPage : ContentPage
+    {
+        readonly LoadingPopupViewModel viewModel;
+        public LoadingPopupPage(string text)
+        {
+            this.InitializeComponent();
+            viewModel = new LoadingPopupViewModel(text);
+            this.BindingContext = viewModel;
+
+        }
+    }
+}
