9826ee0dbefbf91017adab7ce39d00c2b6f6490b
UrielKaralden <urielkaralden@gmail.com>
2020-02-27 19:26:07 +0100

    Added enum to recognize the type of element in RdfElement Model

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/RdfElementType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    public enum RdfElementType
+    {
+        Class,
+        Individual,
+        ObjectProperty,
+        DatatypeProperty,
+        Annotation
+    }
+}
