c9f58e874271eabfd5f8c5ce65605da6b44a6bbc
UrielKaralden <urielkaralden@gmail.com>
2020-03-05 20:37:47 +0100

    Updated SimpleListItem

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/SimpleListItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    public class SimpleListItem
+    {
+        #region Properties
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        #endregion
+
+        #region Constructors
+        public SimpleListItem() { }
+        public SimpleListItem(string name)
+        {
+            ItemName = name;
+        }
+        public SimpleListItem(string name, string description)
+        {
+            ItemName = name;
+            ItemDescription = description;
+        }
+        #endregion
+    }
+}
