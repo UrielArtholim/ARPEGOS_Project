3beed3b011faf1f6051ee5d2c8014d1f81574cd7
UrielKaralden <urielkaralden@gmail.com>
2020-03-05 20:35:42 +0100

    Added DatatypeProperty Element Model

---
+++ SourceCode/ARPEGOS/ARPEGOS/Models/DatatypePropertyElement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARPEGOS.Models
+{
+    public class DatatypePropertyElement : GameElement
+    {
+        #region Properties
+        public Uri OriginElement { get; set; }
+        public dynamic RelatedValue { get; set; }
+        #endregion
+
+        #region Constructor
+        public DatatypePropertyElement(Uri propertyUri, Uri originElementUri, dynamic propertyValue)
+        {
+            var game = SystemControl.ActiveGame;
+            URI = propertyUri;
+            int index = URI.ToString().LastIndexOf('#');
+            Name = URI.ToString().Substring(index);
+            FormattedName = Name.Replace('_', ' ').Replace('#', ' ').Trim();
+            Type = game.GetElementType(URI);
+            OriginElement = originElementUri;
+            RelatedValue = propertyValue;
+        }
+        #endregion
+    }
+}
