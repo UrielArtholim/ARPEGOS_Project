7ee558db9d8c893fe71d114fbc5959656266cc02
urielkaralden <urielkaralden@gmail.com>
2020-06-27 20:08:02 +0200

    Test sidebar

--- SourceCode/ARPEGOS/ARPEGOS/ViewModels/SidebarViewModel.cs
+++ SourceCode/ARPEGOS/ARPEGOS/ViewModels/SidebarViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace ARPEGOS.ViewModels
-{
-    public class SidebarViewModel
-    {
-        public SidebarViewModel()
-        {
+﻿using ARPEGOS.ViewModels.Base;
 
+namespace ARPEGOS.ViewModels
+{
+    public class SidebarViewModel: BaseViewModel
+    {
+        public string SidebarName { get; private set; }
+        public string SidebarValue { get; private set; }
+
+        public SidebarViewModel(string DefaultName = "PD Disponibles",string DefaultValue = "1")
+        {
+            SidebarName = DefaultName;
+            SidebarValue = DefaultValue;
         }
     }
 }
---
+++ SourceCode/ARPEGOS/ARPEGOS/Views/SidebarView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ARPEGOS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ARPEGOS.Views
+{
+    using Autofac;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SidebarView : ContentView
+    {
+        public SidebarView()
+        {
+            InitializeComponent();
+            this.BindingContext = App.Container.Resolve<SidebarViewModel>();
+        }
+    }
+}
