a5c98dc65813e462d1e51d00983cae0a4c378fe3
Diego <diegof@foundation-sp.com>
2020-06-28 17:31:02 +0200

    Step

---
+++ SourceCode/ARPEGOS/ARPEGOS/Controls/CustomSlider.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ARPEGOS.Controls
+{
+    using System;
+
+    using Xamarin.Forms;
+
+    public class CustomSlider : Slider
+    {
+        public static readonly BindableProperty StepValueProperty =
+            BindableProperty.Create(nameof(Step), typeof(int), typeof(CustomSlider), 1, BindingMode.OneTime);
+
+        public CustomSlider()
+        {
+            this.ValueChanged += this.OnSliderValueChanged;
+        }
+
+        public int Step { get; set; }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int newStep = Convert.ToInt32(Math.Round(e.NewValue / this.Step));
+            this.Value = newStep * this.Step;
+        }
+    }
+}
