f7933aec71218939ae7db603ffc30963f2a18950
Diego <diegof@foundation-sp.com>
2020-08-17 22:53:18 +0200

    Add

--- SourceCode/ARPEGOS/ARPEGOS/Services/CharacterOntologyService.Add.cs
+++ SourceCode/ARPEGOS/ARPEGOS/Services/CharacterOntologyService.Add.cs
@@ -32,28 +32,15 @@ namespace ARPEGOS.Services
         /// <param name="subjectFact">Fact that serves as subject of the assertion</param>
         /// <param name="predicate">Datatype property that serves as the predicate of the assertion</param>
         /// <param name="objectFact">Literal that serves as object of the assertion</param>
-        internal void AddDatatypeProperty(string subjectFullName, string predicateFullName, string value, string valuetype)
+        public void AddDatatypeProperty(string subjectFullName, string predicateFullName, string value, string valuetype)
         {
-            RDFOntologyData CharacterDataModel = CharacterOntology.Data;
-            RDFOntologyFact subjectFact;
-            RDFOntologyProperty predicate;
-            RDFOntologyLiteral objectLiteral;
-            string subjectName = subjectFullName.Substring(subjectFullName.LastIndexOf('#') + 1);
-            string predicateName = predicateFullName.Substring(predicateFullName.LastIndexOf('#') + 1);
-
-            if (!CheckIndividual(subjectName))
-                subjectFact = CreateIndividual(subjectName);
-            else
-                subjectFact = CharacterDataModel.SelectFact(subjectFullName);
-
-            if (!CheckDatatypeProperty(predicateName))
-                predicate = CreateDatatypeProperty(predicateName);
-            else
-                predicate = CharacterOntology.Model.PropertyModel.SelectProperty(predicateFullName);
-
-            objectLiteral = CreateLiteral(value, valuetype);
-
-            CharacterDataModel.AddAssertionRelation(subjectFact, predicate as RDFOntologyDatatypeProperty, objectLiteral);
+            var characterDataModel = this.Ontology.Data;
+            var subjectName = subjectFullName.Split('#').Last();
+            var predicateName = predicateFullName.Split('#').Last();
+            var subjectFact = !this.CheckIndividual(subjectName) ? this.CreateIndividual(subjectName) : characterDataModel.SelectFact(subjectFullName);
+            var predicate = !this.CheckDatatypeProperty(predicateName) ? this.CreateDatatypeProperty(predicateName) : this.Ontology.Model.PropertyModel.SelectProperty(predicateFullName);
+            var objectLiteral = this.CreateLiteral(value, valuetype);
+            characterDataModel.AddAssertionRelation(subjectFact, predicate as RDFOntologyDatatypeProperty, objectLiteral);
             this.Save();
         }
     }
