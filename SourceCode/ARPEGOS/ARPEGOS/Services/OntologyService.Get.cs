524314cf72272a498c1a921e40e5fd90b3f49928
Uriel Artholim Karalden <urielkaralden@gmail.com>
2020-08-18 13:26:24 +0200

    Get

---
+++ SourceCode/ARPEGOS/ARPEGOS/Services/OntologyService.Get.cs
@@ -0,0 +1,23 @@
+﻿using RDFSharp.Model;
+using RDFSharp.Semantics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ARPEGOS.Services
+{
+    public partial class OntologyService
+    {
+        /// <summary>
+        /// Returns the root class of the creation scheme of the active game
+        /// </summary>
+        /// <returns></returns>
+        public string GetCreationSchemeRootClass ()
+        {
+            var rootProperty = new RDFOntologyAnnotationProperty(new RDFResource($"{this.Context}{"CreationSchemeRoot"}"));
+            var creationSchemeRootUri = this.Ontology.Model.ClassModel.Annotations.CustomAnnotations.SelectEntriesByPredicate(rootProperty).Single().TaxonomySubject.ToString();
+            return creationSchemeRootUri.Split('#').Last();
+        }
+    }
+}
