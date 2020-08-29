cbcff855d3571ebfa7fa32001fe5082b8b6ad67e
Uriel Artholim Karalden <urielkaralden@gmail.com>
2020-08-29 13:57:42 +0200

    Stage & StageTemplateSelector

--- SourceCode/ARPEGOS/ARPEGOS/Models/Stage.cs
+++ SourceCode/ARPEGOS/ARPEGOS/Models/Stage.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using ARPEGOS.Helpers;
+using ARPEGOS.Services;
+using RDFSharp.Semantics.OWL;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ARPEGOS.Models
 {
@@ -15,17 +20,53 @@ namespace ARPEGOS.Models
 
         public StageType Type { get; private set; }
 
+        public bool IsGrouped { get; private set; }
+
         public ObservableCollection<Item> Items { get; private set; }
 
-        public Stage(string name, IEnumerable<Item> items, StageType stageType = StageType.NotDefined)
+        public Stage (string name, bool grouped = false)
         {
+            var characterService = DependencyHelper.CurrentContext.CurrentCharacter;
             this.Name = name;
-            this.Type = stageType;
+            this.IsGrouped = grouped;
             this.Items = new ObservableCollection<Item>();
-            foreach (var item in items)
-                this.Items.Add(item);
-        }
+            if (this.IsGrouped == true)
+            {
+                var stageItems = characterService.GetIndividualsGrouped(Name);
+                foreach (var item in stageItems)
+                    this.Items.Add(item);
+            }
+            else
+            {
+                var stageItems = characterService.GetIndividuals(Name);
+                foreach (var item in stageItems)
+                    this.Items.Add(item);
+            }
 
+            this.Type = StageType.NotDefined;
+
+            var gameService = DependencyHelper.CurrentContext.CurrentGame;
+            var stageClass = gameService.Ontology.Model.ClassModel.SelectClass($"{gameService.Ontology}{this.Name}");
+            while (this.Type == StageType.NotDefined)
+            {
+                var stageClassCustomAnnotations = gameService.Ontology.Model.ClassModel.Annotations.CustomAnnotations.SelectEntriesBySubject(stageClass);
+                var stageViewTypeAnnotations = stageClassCustomAnnotations.Where(entry => entry.TaxonomyPredicate.ToString().Contains("ViewType"));
+                if (stageViewTypeAnnotations.Count() > 0)
+                {
+                    var viewTypeName = stageViewTypeAnnotations.Single().TaxonomyObject.ToString().Split('^').First();
+                    switch(viewTypeName)
+                    {
+                        case "SingleChoiceListView": this.Type = StageType.SingleChoiceListView; break;
+                        case "MultipleChoiceStaticLimitView": this.Type = StageType.MultipleChoiceStaticLimitView; break;
+                        case "MultipleChoiceStaticLimitGroupCostView": this.Type = StageType.MultipleChoiceStaticLimitGroupCostView; break;
+                        case "MultipleChoiceDynamicLimitView": this.Type = StageType.MultipleChoiceDynamicLimitView; break;
+                        case "ValuedListView": this.Type = StageType.ValuedListView; break;
+                    }
+                }
+                else
+                    stageClass = gameService.Ontology.Model.ClassModel.Relations.SubClassOf.SelectEntriesBySubject(stageClass).Single().TaxonomyObject as RDFOntologyClass;
+            }
+        }
         public enum StageType
         {
             SingleChoiceListView,
--- SourceCode/ARPEGOS/ARPEGOS/Templates/TemplateSelectors/StageTemplateSelector.cs
+++ SourceCode/ARPEGOS/ARPEGOS/Templates/TemplateSelectors/StageTemplateSelector.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using ARPEGOS.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ARPEGOS.Templates.TemplateSelectors
 {
-    public class StageTemplateSelector
+    public class StageTemplateSelector: DataTemplateSelector
     {
+        public DataTemplate SingleChoiceListTemplate { get;set; }
+        public DataTemplate MultipleChoiceStaticLimitTemplate { get; set; }
+        public DataTemplate MultipleChoiceStaticLimitGroupCostTemplate { get; set; }
+        public DataTemplate MultipleChoiceDynamicLimitTemplate { get; set; }
+        public DataTemplate ValuedListTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate (object item, BindableObject container)
+        {
+            var stage = item as Stage;
+            switch(stage.Type)
+            {
+                case Stage.StageType.MultipleChoiceStaticLimitView: return MultipleChoiceStaticLimitTemplate;
+                case Stage.StageType.MultipleChoiceStaticLimitGroupCostView: return MultipleChoiceStaticLimitGroupCostTemplate;
+                case Stage.StageType.MultipleChoiceDynamicLimitView: return MultipleChoiceDynamicLimitTemplate;
+                case Stage.StageType.ValuedListView: return ValuedListTemplate;
+                default: return SingleChoiceListTemplate;
+            }
+
+        }
     }
 }
