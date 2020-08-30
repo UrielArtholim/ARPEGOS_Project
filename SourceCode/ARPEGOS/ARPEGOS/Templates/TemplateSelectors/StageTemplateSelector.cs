c7b5f9bb295479dba2944ed19b8b76413181b16c
Uriel Artholim Karalden <urielkaralden@gmail.com>
2020-08-30 15:42:42 +0200

    Revert "Stage & StageTemplateSelector"
    This reverts commit cbcff855d3571ebfa7fa32001fe5082b8b6ad67e.

--- SourceCode/ARPEGOS/ARPEGOS/Templates/TemplateSelectors/StageTemplateSelector.cs
+++ SourceCode/ARPEGOS/ARPEGOS/Templates/TemplateSelectors/StageTemplateSelector.cs
@@ -1,31 +1,10 @@
-﻿using ARPEGOS.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Xamarin.Forms;
 
 namespace ARPEGOS.Templates.TemplateSelectors
 {
-    public class StageTemplateSelector: DataTemplateSelector
+    public class StageTemplateSelector
     {
-        public DataTemplate SingleChoiceListTemplate { get;set; }
-        public DataTemplate MultipleChoiceStaticLimitTemplate { get; set; }
-        public DataTemplate MultipleChoiceStaticLimitGroupCostTemplate { get; set; }
-        public DataTemplate MultipleChoiceDynamicLimitTemplate { get; set; }
-        public DataTemplate ValuedListTemplate { get; set; }
-
-        protected override DataTemplate OnSelectTemplate (object item, BindableObject container)
-        {
-            var stage = item as Stage;
-            switch(stage.Type)
-            {
-                case Stage.StageType.MultipleChoiceStaticLimitView: return MultipleChoiceStaticLimitTemplate;
-                case Stage.StageType.MultipleChoiceStaticLimitGroupCostView: return MultipleChoiceStaticLimitGroupCostTemplate;
-                case Stage.StageType.MultipleChoiceDynamicLimitView: return MultipleChoiceDynamicLimitTemplate;
-                case Stage.StageType.ValuedListView: return ValuedListTemplate;
-                default: return SingleChoiceListTemplate;
-            }
-
-        }
     }
 }
