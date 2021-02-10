96d7b22727053b0e34f54c46b72f8d2033fbc3a0
UrielKaralden <urielkaralden@gmail.com>
2021-02-10 13:40:08 +0100

    Added Game images

--- SourceCode/ARPEGOS/ARPEGOS/Converters/GetInitialsImageConverter.cs
+++ SourceCode/ARPEGOS/ARPEGOS/Converters/GetInitialsImageConverter.cs
@@ -18,8 +18,8 @@ namespace ARPEGOS.Converter
                 var length = Math.Max(Math.Min(name.Split(' ').Length, 5), 2);
                 var size = new[] { 0.5, 0.4, 0.35, 0.3 };
                 return name != string.Empty
-                           ? $"https://ui-avatars.com/api/?background=424953&color=eee&name={name}&size={imageSize}&rounded=true&length={length}&uppercase=false&font-size={size[length - 2]}"
-                           : $"https://ui-avatars.com/api/?background=4899de&color=fff&name=crear&size={imageSize}&rounded=true&length=5&uppercase=false&font-size={size[5 - 2]}";
+                           ? $"https://ui-avatars.com/api/?background=424953&color=eee&name={name}&size={imageSize}&rounded=true&length={length}&uppercase=false&font-size={size[length - 2]}" : string.Empty;
+                           //: $"https://ui-avatars.com/api/?background=4899de&color=fff&name=crear&size={imageSize}&rounded=true&length=5&uppercase=false&font-size={size[5 - 2]}";
             }
 
             return null;
